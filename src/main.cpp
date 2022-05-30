0baac81f4b9bd9253a19c9feb6fc31539440fa16
alanjian85 <alanjian85@outlook.com>
2022-05-30 14:15:58 +0800

    Simple ImGui window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,51 @@
-#include <iostream>
+#include <GLFw/glfw3.h>
+#include <imgui.h>
+#include "../bindings/imgui_impl_glfw.h"
+#include "../bindings/imgui_impl_opengl3.h"
 
 int main() {
-    std::cout << "Hello world!\n" << std::endl;
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
+    glfwInit();
+
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "ImLog", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    ImGui::StyleColorsDark();
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+            ImGui::Begin("Test");
+                ImGui::Text("Hello world!");
+            ImGui::End();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
