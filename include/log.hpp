eff325b47d8981755182bc00bf06113a8bb7a8be
alanjian85 <alanjian85@outlook.com>
2022-06-01 16:43:26 +0800

    Add log helper methods

--- include/log.hpp
+++ include/log.hpp
@@ -1,12 +1,32 @@
 #ifndef LOG_HPP
 #define LOG_HPP
 
+#include <vector>
+
 #include <imgui.h>
 
 class Log {
 public:
     Log() {
         m_AutoScroll = true;
+        clear();
+    }
+
+    void clear() {
+        m_Buffer.clear();
+        m_LineOffsets.clear();
+        m_LineOffsets.push_back(0);
+    }
+
+    void addLog(const char* str) {
+        int oldSize = m_Buffer.size();
+        m_Buffer.append(str);
+        int newSize = m_Buffer.size();
+        for (; oldSize < newSize; ++oldSize) {
+            if (m_Buffer[oldSize] == '\n') {
+                m_LineOffsets.push_back(oldSize + 1);
+            }
+        }
     }
 
     void draw(const char* title, bool* open = nullptr) {
@@ -28,10 +48,16 @@ public:
             ImGui::SameLine();
             m_Filter.Draw("Filter", -50.0f);
 
+            ImGui::Separator();
+            ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
+                
+            ImGui::EndChild();
         ImGui::End();
     }
 private:
+    ImGuiTextBuffer m_Buffer;
     ImGuiTextFilter m_Filter;
+    std::vector<int> m_LineOffsets;
     bool m_AutoScroll;
 };
 
